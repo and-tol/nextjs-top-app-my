69ffd36672b105d37c7082924d6e4450f7842944
Andrei Tolstoi <tolstoi.a@gmail.com>
2021-08-16 12:31:25 +0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Create Next</title>
+        <link key={1} rel='icon' href='/favicon2.ico' />
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet"/>
+      </Head>
+      <Component {...pageProps} />;
+    </>);
 }
-export default MyApp
+export default MyApp;
